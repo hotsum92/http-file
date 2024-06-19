baa9d88e51430983c01af7e84ca6302312e67f9a
832022 <hotsuma_kawashima@densan-s.co.jp>
2024-06-19 18:27:14 +0900

    update

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -23,6 +26,44 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		fmt.Println(p.FormName(), ":", p.FileName(), p.Header.Get("Content-Type"))
+
+		// ファイルを読み込む
+		buf, err := ioutil.ReadAll(p)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		unzip(buf)
+
+	}
+}
+
+func unzip(file []byte) {
+	// zipファイルを読み込む
+	r, err := zip.NewReader(bytes.NewReader(file), int64(len(file)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 各ファイルを展開する
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rc.Close()
+
+		// ファイル名を出力する
+		fmt.Println(f.Name)
+
+		// ファイルを展開する
+		buf, err := ioutil.ReadAll(rc)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// ファイルの内容を出力する
+		fmt.Println(string(buf))
 	}
 }
 
